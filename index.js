a04a4d551291e480923959c52beed23154e9a168
SantiagoHitta <santiagohitta@gmail.com>
2015-10-03 21:26:25 +0200

    h

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
  * Copyright (c) 2014-2015, Facebook, Inc.
- * All rights reservedssssa
+ * All rights reservedssssahhh
  *
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree. An additional grant
