bc92f8215e60d3ec5b551c9d8786af4df2915292
SantiagoHitta <santiagohitta@gmail.com>
2015-10-03 21:24:49 +0200

    cambie 1 boludez

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
  * Copyright (c) 2014-2015, Facebook, Inc.
- * All rights reserved.
+ * All rights reservedssssa
  *
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree. An additional grant
